67aed50158958b3fbaf28ba3989ce215b7a1aaa1
shammi77 <nusratshammi.vutu@gmail.com>
2022-05-24 01:58:47 +0600

    portfolio add

--- src/pages/MyPortfolio.js
+++ src/pages/MyPortfolio.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const MyPortfolio = () => {
     return (
-        <div>
-            <h1>portfolio</h1>
+        <div className="text-center m-5 text-2xl">
+            <h1>Nusrat Jahan Shammi</h1>
+            <h3>E-Mail : nusratshammi.vutu@gmail.com</h3>
+            <div className="divider"></div> 
+            <h4>Educational Background :< br />
+            
+            Current : B. sc in CSE at Leading University , Sylhet, Bangladesh.
+            <br/>
+            HSC: Women's Model College(Science, GPA-4.17)<br/>
+            SSC: Isamoti High School(Science, GPA-4.72)
+             </h4>
+             <div className="divider"></div> 
+             <h4>Technical Skill: </h4>
+             <h4>Front-end :  React Js, React Router, DOM(V6), JavaScript, bootstrap, tailwind, CSS, HTML</h4>
+             <h4>Back-end:  Node.js, Express, Mongodb</h4>
+             <h4>CMS:  WordPress</h4>
+             <div className="divider"></div> 
+             <h4>My 3 Live Sites Link:</h4>
+                <h5> 1 . https://resilient-dodol-1009b3.netlify.app/</h5>
+                <h5> 2 . https://cocky-swanson-8cfd6b.netlify.app/</h5>
+                <h5></h5>
         </div>
     );
 };
